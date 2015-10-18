e3cc49d39bea3c642229314fc4e767b55f438956
niels <nm@9la.dk>
2015-10-18 18:09:34 +0200

    App now connects to MongoDB on startup.

--- express-mongodb-rabbitmq/app.js
+++ express-mongodb-rabbitmq/app.js
@@ -2,6 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var cors = require('cors');
+var mongoose = require('mongoose');
+
+// Configure data models
+var models = require("./models/data_model")(mongoose);
 
 // Configure web server
 var app = express()
@@ -12,10 +16,23 @@ var app = express()
 var http = require('http').Server(app);
 
 // Initialize services
-var moment_service = require('./services/moment_service')(app);
-var chat_service = require('./services/chat_service')(app);
+var moment_service = require('./services/moment_service')(app, models);
+// FIXME: Enable the chat service
+//var chat_service = require('./services/chat_service')(app);
 
-// Start listening
-http.listen(1337, function() {
-    console.log('Server running at http://127.0.0.1:1337/');
+// Connect to database
+var db = mongoose.connection;
+mongoose.connect('mongodb://localhost/express-mongodb-rabbitmq');
+
+// On failure to connect, abort server startup and show error
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// On successful connection, finalize server startup
+db.once('open', function() {
+    console.log("Connected to database successfully!");
+
+    // Start web server
+    http.listen(1337, function() {
+        console.log('Server running at http://127.0.0.1:1337/');
+    });
 });
