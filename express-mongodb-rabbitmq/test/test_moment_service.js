84c344d6c0e940f15450df5dcaf1ade8d3b16669
niels <nm@9la.dk>
2015-10-20 18:38:01 +0200

    Group tests by function.

--- express-mongodb-rabbitmq/test/test_moment_service.js
+++ express-mongodb-rabbitmq/test/test_moment_service.js
@@ -35,105 +35,113 @@ describe('Moment service', function() {
             .then(null, function(err) { done(err) });
     });
 
-    it('returns a moment based on the id', function(done) {
-        var moment = testData[0];
-        var momentId = moment._id.toString();
-        req.get('/moment/' + momentId)
-            .on('data', function(data) {
-                var response = this.response;
-                expect(response.statusCode).to.equal(200);
+    describe('Collection - POST', function() {
+        it('creates a moment upon valid POST', function(done) {
+            var moment = {
+                authorName: "John Doe",
+                text: "This is a beautiful message"
+            };
+            req.post('/moment', { body:moment })
+                .on('data', function(data) {
+                    var response = this.response;
+                    expect(response.statusCode).to.equal(200);
 
-                var body = JSON.parse(data);
-                expect(body.authorName).to.equal(moment.authorName);
-                expect(body.text).to.equal(moment.text);
-                expect(body._id).to.be.a("String").and.have.length(24);
-                expect(body.created).to.be.a("String").and.have.length(24);
-                expect(body.modified).to.be.a("String").and.have.length(24);
-                expect(body.commentCount).to.equal(0);
-                done();
-            })
-            .on('error', done);
+                    var body = JSON.parse(data);
+                    expect(body.authorName).to.equal(moment.authorName);
+                    expect(body.text).to.equal(moment.text);
+                    expect(body._id).to.be.a("String").and.have.length(24);
+                    expect(body.created).to.be.a("String").and.have.length(24);
+                    expect(body.modified).to.be.a("String").and.have.length(24);
+                    expect(body.commentCount).to.equal(0);
+                    done();
+                })
+                .on('error', done);
+        });
     });
 
-    it('returns "Not Found" upon an unknown ID', function(done) {
-        req.get('/moment/5623b13f12618795514b39cd')
-            .on('response', function(response) {
-                expect(response.statusCode).to.equal(404);
-                done();
-            })
-            .on('error', done);
+    describe('Element - GET', function() {
+        it('returns a moment based on the id', function(done) {
+            var moment = testData[0];
+            var momentId = moment._id.toString();
+            req.get('/moment/' + momentId)
+                .on('data', function(data) {
+                    var response = this.response;
+                    expect(response.statusCode).to.equal(200);
+
+                    var body = JSON.parse(data);
+                    expect(body.authorName).to.equal(moment.authorName);
+                    expect(body.text).to.equal(moment.text);
+                    expect(body._id).to.be.a("String").and.have.length(24);
+                    expect(body.created).to.be.a("String").and.have.length(24);
+                    expect(body.modified).to.be.a("String").and.have.length(24);
+                    expect(body.commentCount).to.equal(0);
+                    done();
+                })
+                .on('error', done);
+        });
+
+        it('returns "Not Found" upon an unknown ID', function(done) {
+            req.get('/moment/5623b13f12618795514b39cd')
+                .on('response', function(response) {
+                    expect(response.statusCode).to.equal(404);
+                    done();
+                })
+                .on('error', done);
+        });
     });
 
-    it('creates a moment upon valid POST', function(done) {
-        var moment = {
-            authorName: "John Doe",
-            text: "This is a beautiful message"
-        };
-        req.post('/moment', { body:moment })
-            .on('data', function(data) {
-                var response = this.response;
-                expect(response.statusCode).to.equal(200);
+    describe('Element - PUT', function() {
+        it('updates a moments text upon valid UPDATE', function(done) {
+            var momentId = testData[0]._id;
+            var moment = {
+                text: "This is a beautiful message"
+            };
+            req.put('/moment/' + momentId, { body:moment })
+                .on('response', function(response) {
 
-                var body = JSON.parse(data);
-                expect(body.authorName).to.equal(moment.authorName);
-                expect(body.text).to.equal(moment.text);
-                expect(body._id).to.be.a("String").and.have.length(24);
-                expect(body.created).to.be.a("String").and.have.length(24);
-                expect(body.modified).to.be.a("String").and.have.length(24);
-                expect(body.commentCount).to.equal(0);
-                done();
-            })
-            .on('error', done);
+                })
+                .on('data', function(data) {
+                    var response = this.response;
+                    expect(response.statusCode).to.equal(200);
+
+                    var body = JSON.parse(data);
+                    expect(body.authorName).to.equal(testData[0].authorName);
+                    expect(body.text).to.equal(moment.text);
+                    expect(body.modified).to.not.equal(testData[0].modified);
+                    expect(body.created).to.equal(testData[0].created);
+                    done();
+                })
+                .on('error', done);
+        });
     });
 
-    it('updates a moments text upon valid UPDATE', function(done) {
-        var momentId = testData[0]._id;
-        var moment = {
-            text: "This is a beautiful message"
-        };
-        req.put('/moment/' + momentId, { body:moment })
-            .on('response', function(response) {
+    describe('Element - DELETE', function() {
+        it('deletes a moment upon valid DELETE', function(done) {
+            var moment = testData[0];
+            var momentId = moment._id.toString();
+            req.del('/moment/' + momentId)
+                .on('data', function(data) {
+                    var response = this.response;
+                    var body = JSON.parse(data);
+                    expect(response.statusCode).to.equal(200);
+                    expect(body.authorName).to.equal(moment.authorName);
+                    expect(body.text).to.equal(moment.text);
+                    expect(body._id).to.be.a("String").and.have.length(24);
+                    expect(body.created).to.be.a("String").and.have.length(24);
+                    expect(body.modified).to.be.a("String").and.have.length(24);
+                    expect(body.commentCount).to.equal(0);
 
-            })
-            .on('data', function(data) {
-                var response = this.response;
-                expect(response.statusCode).to.equal(200);
-
-                var body = JSON.parse(data);
-                expect(body.authorName).to.equal(testData[0].authorName);
-                expect(body.text).to.equal(moment.text);
-                expect(body.modified).to.not.equal(testData[0].modified);
-                expect(body.created).to.equal(testData[0].created);
-                done();
-            })
-            .on('error', done);
-    });
-
-    it('deletes a moment upon valid DELETE', function(done) {
-        var moment = testData[0];
-        var momentId = moment._id.toString();
-        req.del('/moment/' + momentId)
-           .on('data', function(data) {
-               var response = this.response;
-               var body = JSON.parse(data);
-               expect(response.statusCode).to.equal(200);
-               expect(body.authorName).to.equal(moment.authorName);
-               expect(body.text).to.equal(moment.text);
-               expect(body._id).to.be.a("String").and.have.length(24);
-               expect(body.created).to.be.a("String").and.have.length(24);
-               expect(body.modified).to.be.a("String").and.have.length(24);
-               expect(body.commentCount).to.equal(0);
-
-               MongoClient.connect(mongoCS)
-                   .then(function(db) {
-                       return db.collection("moments").find().toArray()
-                   })
-                   .then(function(moments) {
-                       expect(moments).to.have.length(1);
-                       expect(moments[0]._id.toString()).to.equal(testData[1]._id.toString());
-                       done();
-                   });
-           })
-           .on('error', done);
+                    MongoClient.connect(mongoCS)
+                        .then(function(db) {
+                            return db.collection("moments").find().toArray()
+                        })
+                        .then(function(moments) {
+                            expect(moments).to.have.length(1);
+                            expect(moments[0]._id.toString()).to.equal(testData[1]._id.toString());
+                            done();
+                        });
+                })
+                .on('error', done);
+        });
     });
 });
